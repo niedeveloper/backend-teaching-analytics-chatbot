00f8d6d18dfce3c507c4f4347d5cca8338a31998
niedeveloper <niedeveloper1@gmail.com>
2026-04-23 09:44:37 +1000

    f: allow origins config

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="NIE Backend API", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://niefrontend.reallycoolworkspace.uk"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
